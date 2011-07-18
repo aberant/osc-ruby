8d161507b7a13ccffa6cf99883321c9f87234d2e
aberant <qzzzq1@gmail.com>
2011-07-18 18:28:09 -0500

    implementing experimental double splat matcher

--- lib/osc-ruby/address_pattern.rb
+++ lib/osc-ruby/address_pattern.rb
@@ -2,43 +2,46 @@ module OSC
   class AddressPattern
     def initialize( pattern )
       @pattern = pattern
-      
+
       generate_regex_from_pattern
     end
-    
+
     def match?( address )
       !!(@re.nil? || @re.match( address ))
     end
-    
-private 
+
+private
     def generate_regex_from_pattern
       case @pattern
         when NIL; @re = @pattern
         when Regexp; @re = @pattern
         when String
-          
+
           # i'm unsure what this does
           # @pattern.gsub!(/[.^(|)]/, '\\1')
-          
+
           # handles osc single char wildcard matching
           @pattern.gsub!(/\?/, '[^/]')
-          
+
+          # handles ** - used in creating osc routers
+          @pattern.gsub!(/\*\*/, '.*' )
+
           # handles osc * - 0 or more matching
-          @pattern.gsub!(/\*/, '[^/]*')
-          
+          @pattern.gsub!(/\*[^\*]/, '[^/]*')
+
           # handles [!] matching
           @pattern.gsub!(/\[!/, '[^')
-            
+
           # handles {} matching
           @pattern.gsub!(/\{/, '(')
           @pattern.gsub!(/,/, '|')
           @pattern.gsub!(/\}/, ')')
-          
-          
+
+
           # keeps from matching before the begining of the pattern
           @pattern.gsub!(/\A/, '\A')
-          
-          # keeps from matching beyond the end, 
+
+          # keeps from matching beyond the end,
           # eg. pattern /hi does not match /hidden
           @pattern.gsub!(/\z/, '\z')
 
