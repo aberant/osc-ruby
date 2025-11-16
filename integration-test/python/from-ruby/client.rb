eb660c8755459f4ed003021884d9549de5062e29
aberant <qzzzq1@gmail.com>
2025-11-16 16:41:36 -0600

    adding bundle to from-ruby test

--- integration-test/python/from-ruby/client.rb
+++ integration-test/python/from-ruby/client.rb
@@ -3,12 +3,24 @@ require 'bundler/setup'
 $:.unshift File.join( File.dirname( __FILE__ ), '..', '..', '..', 'lib')
 require 'osc-ruby/client'
 require 'osc-ruby/message'
+require 'osc-ruby/bundle'
 
-@client = OSC::Client.new('localhost', 3333)
+immediate = nil
 
-@client.send(OSC::Message.new("/test", 42))
-@client.send(OSC::Message.new("/test", OSC::OSCFloat32.new(3.14159)))
-@client.send(OSC::Message.new("/test", "Hello, World!"))
-@client.send(OSC::Message.new("/test", nil))
-@client.send(OSC::Message.new("/test", true))
-@client.send(OSC::Message.new("/test", false))
+client = OSC::Client.new('localhost', 3333)
+int_msg = OSC::Message.new("/test", 42)
+float_msg = OSC::Message.new("/test", OSC::OSCFloat32.new(3.14159))
+string_msg = OSC::Message.new("/test", "Hello, World!")
+nil_msg = OSC::Message.new("/test", nil)
+true_msg = OSC::Message.new("/test", true)
+false_msg = OSC::Message.new("/test", false)
+
+client.send(int_msg)
+client.send(float_msg)
+client.send(string_msg)
+client.send(nil_msg)
+client.send(true_msg)
+client.send(false_msg)
+
+bundle = OSC::Bundle.new(immediate, int_msg, float_msg, string_msg, nil_msg, true_msg, false_msg)
+client.send(bundle)
